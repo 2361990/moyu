669e99040a3ce506d07952f801b47024ffe3423c
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2022-11-12 02:16:12 +0800

    Delete Html directory

--- Html/JS/cupbox.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@master/Html/CSS/cupbox.css" type="text/css">');
-$done({ body });
