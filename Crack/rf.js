a9237495a3e13905a4595018117f9d425220ad95
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2022-11-12 11:28:35 +0800

    Add files via upload

--- Crack/rf.js
+++ Crack/rf.js
@@ -1,18 +1,11 @@
 /*
 
-应用名称：RNI films
-软件版本：3.0.5
-下载地址：https://shrtm.nu/ZGyR
-脚本作者：Cuttlefish
-微信账号：墨鱼手记
-更新时间：2022-02-12
 通知频道：https://t.me/ddgksf2021
 问题反馈：https://t.me/ddgksf2013_bot
 
 [rewrite_local]
 
-# ～ RNI解锁高级会员（20220212）@ddgksf2013
-https://pro-status-service-prod.azurewebsites.net/api/item url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/rf.js
+https://pro-status-service-prod.azurewebsites.net/api/item url script-response-body https://codeberg.org/ddgksf2013/Cuttlefish/raw/branch/master/Crack/rf.js
 
 [mitm] 
 
